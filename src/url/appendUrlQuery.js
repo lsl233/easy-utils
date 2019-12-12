293f705bcba15fb3a2319fab8db7cf14a9fda38d
lishilei <lishilei@meb.com>
2019-12-12 10:51:46 +0800

    function appendUrlQuery

--- src/url/appendUrlQuery.js
+++ src/url/appendUrlQuery.js
@@ -1,8 +1,14 @@
 import isObjectData from '../object/isObjectData'
 
+/**
+ * 增加URL参数
+ * @param url {string} URL地址（没有验证URL合法性）
+ * @param query {object} 扩展参数
+ * @returns {string} 扩展后的URL
+ */
 export default function appendUrlQuery (url, query) {
-	const hasQuery = url.indexOf('?') > -1
 	if (isObjectData(query)) {
+		const hasQuery = url.indexOf('?') > -1
 		let index = 0
 		for (const key in query) {
 			if (!query.hasOwnProperty(key)) continue
@@ -15,6 +21,5 @@ export default function appendUrlQuery (url, query) {
 			index++
 		}
 	}
-
 	return url
 }
--- test/url/appendUrlQuery.test.js
+++ test/url/appendUrlQuery.test.js
@@ -1,8 +1,19 @@
 import { appendUrlQuery } from '../../src/url'
 
 describe('append url query', () => {
-	test('appendUrlQuery', () => {
+	test('appendUrlQuery(\'https://github.com\', { id: \'123\' })', () => {
 		expect(appendUrlQuery('https://github.com', { id: '123' })).toBe('https://github.com?id=123')
+	})
+
+	test('appendUrlQuery(\'https://github.com?key=1\', { id: \'123\' })', () => {
 		expect(appendUrlQuery('https://github.com?key=1', { id: '123' })).toBe('https://github.com?key=1&id=123')
+	})
+
+	test('appendUrlQuery(\'https://github.com\', {})', () => {
+		expect(appendUrlQuery('https://github.com', {})).toBe('https://github.com')
+	})
+
+	test('appendUrlQuery(\'https://github.com\')', () => {
+		expect(appendUrlQuery('https://github.com')).toBe('https://github.com')
 	})
 })
